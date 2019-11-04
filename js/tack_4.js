73cc50663d7276cbc09d18a5365e03255ac2cb33
borys.pidperygora <borys.pidperygora@forte.net>
2019-11-04 17:53:16 +0200

    Add tack 4

--- js/tack_4.js
+++ js/tack_4.js
@@ -1,21 +1,36 @@
 let input_field = document.querySelector('.input_field input');
-let output = document.querySelector('.output_field p');
+let output = document.querySelector('.output_field p .is');
 
-$(input_field).keyup(function (e) {
-
-    console.log(input_field.value);
-    let number = [];
-
-    for (let j = 0; j < number.length; j++) {
-        for (let i = 0; i < number.length; i++) {
-            if (number[i] > number[i + 1]) {
-                let AlphaNumber = number[i];
-                number[i] = number[i + 1];
-                number[i + 1] = AlphaNumber;
-            }
-
+function CheckNumeric(e) {
+    if (window.event) // IE
+    {
+        if ((e.keyCode < 48 || e.keyCode > 57) & e.keyCode != 8 && e.keyCode != 44) {
+            event.returnValue = false;
+            return false;
         }
     }
-    output.innerHTML = number;
-});
+    else { // Fire Fox
+        if ((e.which < 48 || e.which > 57) & e.which != 8 && e.which != 44) {
+            e.preventDefault();
+            return false;
+        }
+    }
+}
+function sortIt(){
+
+    let number = input_field.value.split(",");
+
+        console.log(number);
+
+        for (let j = 0; j < number.length; j++) {
+            for (let i = 0; i < number.length; i++) {
+                if (parseInt(number[i]) > parseInt(number[i + 1])) {
+                    let AlphaNumber = number[i];
+                    number[i] = number[i + 1];
+                    number[i + 1] = AlphaNumber;
+                }
+            }
+        }
+        output.innerHTML = number.join(', ');
+};
 
